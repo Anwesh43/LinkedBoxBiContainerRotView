2424ae54327568f4a89063aea04c993486f3ec37
Anwesh43 <anweshthecool0@gmail.com>
2021-06-14 09:10:29 +0530

    adding BoxBiCotainerRotView class

--- boxbicontainerrotview/src/main/java/com/example/boxbicontainerrotview/BoxBiCotainerRotView.kt
+++ boxbicontainerrotview/src/main/java/com/example/boxbicontainerrotview/BoxBiCotainerRotView.kt
@@ -71,3 +71,19 @@ fun Canvas.drawBBCRNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBoxBiContainerRot(scale, w, h, paint)
 }
+
+class BoxBiCotainerRotView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}

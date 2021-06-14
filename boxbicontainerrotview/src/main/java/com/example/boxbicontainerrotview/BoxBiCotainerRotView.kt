a5881e72577fd6f71cdfc7c1b7a63e3ea0591935
Anwesh43 <anweshthecool0@gmail.com>
2021-06-14 10:00:46 +0530

    adding BBCRNode class

--- boxbicontainerrotview/src/main/java/com/example/boxbicontainerrotview/BoxBiCotainerRotView.kt
+++ boxbicontainerrotview/src/main/java/com/example/boxbicontainerrotview/BoxBiCotainerRotView.kt
@@ -137,4 +137,45 @@ class BoxBiCotainerRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BBCRNode(var i : Int, val state : State = State()) {
+
+        private var next : BBCRNode? = null
+        private var prev : BBCRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = BBCRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBBCRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BBCRNode {
+            var curr : BBCRNode ?= prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
